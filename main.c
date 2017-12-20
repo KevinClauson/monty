1eac9ad087d8ef39db73e2dd7e3ff33fd01136ce
KevinClauson <kevinclauson99@gmail.com>
2017-12-20 16:36:58 +0000

    main and header file

---
+++ main.c
@@ -0,0 +1,85 @@
+#include "monty.h"
+
+void error_handler(int error, char **argv)
+{
+	switch(error)
+	{
+	case 1 :
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	case 2 :
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	case 3:
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	default:
+		printf("Not sure why this failed, but please start over.\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+char **parse_args(char *line)
+{
+	size_t i = 0, len = BUF_SIZE;
+	char *token;
+	char **args = malloc(len * sizeof(char *));
+	/*if (args == NULL)
+	  error_handler(3, );*/
+	token = strtok(line, " ");
+	while(token != NULL)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+	return (args);
+}
+
+
+void read_file(FILE *fp)
+{
+	ssize_t read;
+	size_t len = 0;
+	int i;
+	char *line = NULL, **args;
+	int line_num = 0;
+	while ((read = getline(&line, &len, fp)) != -1)
+	{
+		++line_num;
+		printf("%d, %s", line_num, line);
+		args = parse_args(line);
+		i = 0;
+		while (args[i])
+		{
+			printf("args[%d]: %s\n", i, args[i]);
+			++i;
+		}
+	}
+	fclose(fp);
+	exit(EXIT_SUCCESS);
+}
+
+
+
+/**
+ * main - main function that controls the whole monty program
+ * @argc: argument count
+ * @argument: array of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+
+	if (argc != 2)
+		error_handler(1, argv);
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+		error_handler(2, argv);
+	read_file(fp);
+
+	return (0);
+}
---
+++ monty.h
@@ -0,0 +1,62 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+/******* Libraries ********/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/****** Defintions ********/
+#define BUF_SIZE 1024
+
+/******* Structures *******/
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct data - caries data around to use in various functions
+ * @file_name: the file name reading from
+ *
+ */
+typedef struct data_s
+{
+	char *file_name;
+
+} data_t;
+
+#endif
