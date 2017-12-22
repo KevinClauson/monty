2422bde2056bf44d6d4c48964d1e154a349a4b8b
KevinClauson <kevinclauson99@gmail.com>
2017-12-22 02:01:50 +0000

    update to error handler for subtract

--- error_handler.c
+++ error_handler.c
@@ -47,3 +47,45 @@ void error_handler(int error)
 	}
 	exit(EXIT_FAILURE);
 }
+
+void error_handler_2(int error)
+{
+	switch (error)
+	{
+	case 1:
+		printf("L%d: can't sub, stack too short\n", data_g->line_num);
+		break;
+	case 2:
+		printf("Error: Can't open file %s\n", data_g->file_name);
+		break;
+	case 3:
+		printf("Error: malloc failed\n");
+		break;
+	case 4:
+		printf("L%d: unknown instruction %s\n", data_g->line_num, data_g->args[0]);
+		break;
+	case 5:
+		printf("L%d: usage: push integer\n", data_g->line_num);
+		break;
+	case 6:
+		printf("L%d: can't pint, stack empty\n", data_g->line_num);
+		break;
+	case 7:
+		printf("L%d: can't pop an empty stack\n", data_g->line_num);
+		break;
+	case 8:
+		printf("L%d: can't swap, stack too short\n", data_g->line_num);
+		break;
+	case 9:
+		printf("L%d: can't add, stack too short\n", data_g->line_num);
+		break;
+	default:
+		printf("Not sure why this failed, but please start over.\n");
+		exit(EXIT_FAILURE);
+	}
+	if (data_g != NULL)
+	{
+		free_data();
+	}
+	exit(EXIT_FAILURE);
+}
