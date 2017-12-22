37cfff7b46c1fe57cade4a7afc7633d378a359be
KevinClauson <kevinclauson99@gmail.com>
2017-12-22 22:52:33 +0000

    change back

--- opcode_func_1.c
+++ opcode_func_1.c
@@ -19,21 +19,14 @@ void my_push(stack_t **stack, unsigned int line_number)
 		error_handler(3);
 	data_g->argument_2 = atoi(data_g->args[1]);
 	new->n = data_g->argument_2;
-	if (data_g->lifo == 0)
-	{
-		if (stack == NULL)
-			new->next = NULL;
-		else
-			new->next = *stack;
-		if (*stack != NULL)
-			(*stack)->prev = new;
-		data_g->stack = new;
-		++data_g->stack_len;
-	}
+	if (stack == NULL)
+		new->next = NULL;
 	else
-	{
-		printf("your fucked\n");
-	}
+		new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	data_g->stack = new;
+	++data_g->stack_len;
 }
 
 /**
